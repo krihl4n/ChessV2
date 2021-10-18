5b0453b0b5e1ed20a0bf1dd7a615cd5605f69437
krihl4n <lkolasa1989@gmail.com>
2021-10-18 19:57:38 +0200

    refactor tests

---
+++ src/test/groovy/com/krihl4n/CapturesSpec.groovy
@@ -0,0 +1,33 @@
+package com.krihl4n
+
+class CapturesSpec extends BaseGameSpec {
+
+    void setup() {
+        game.start()
+    }
+
+    def "should perform attack"() {
+        given:
+        setupPieces("wr_c3 bp_c5")
+
+        when:
+        game.performMove("c3", "c5")
+
+        then:
+        assertPositions("wr_c5")
+    }
+
+    def "should undo attack"() {
+        given:
+        setupPieces("wr_c3 bp_c5")
+
+        and:
+        game.performMove("c3", "c5")
+
+        when:
+        game.undoMove()
+
+        then:
+        assertPositions("wr_c3 bp_c5")
+    }
+}
