2f916b240bdba08368d3169d502dc80b7187565b
krihl4n <lkolasa1989@gmail.com>
2021-12-03 22:49:31 +0100

    test for castling

--- src/test/groovy/com/krihl4n/CastlingSpec.groovy
+++ src/test/groovy/com/krihl4n/CastlingSpec.groovy
@@ -43,67 +43,29 @@ class CastlingSpec extends BaseGameSpec {
         "black king short" | "bk_e8 br_h8" | "e8 g8" || "bk_e8 br_h8"
     }
 
-    def "cannot do white king short castle if rook moved before"() {
+    def "can't do castling of type #type if rook moved before"() {
         given:
-        setupPieces("wk_e1 wr_h1")
+        setupPieces(setup)
 
         and:
-        performMove("h1 h2")
-        performMove("h2 h1")
+        performMove(rookMove1)
+        performMove(rookMove2)
 
         when:
-        performMove("e1 g1")
+        performMove(castlingMove)
 
         then:
-        assertPositions("wk_e1 wr_h1")
+        assertPositions(positions)
+
+        where:
+        type               | setup         | rookMove1 | rookMove2 | castlingMove || positions
+        "white king long"  | "wk_e1 wr_a1" | "a1 a2"   | "a2 a1"   | "e1 c1"      || setup
+        "white king short" | "wk_e1 wr_h1" | "h1 h2"   | "h2 h1"   | "e1 g1"      || setup
+        "black king long"  | "bk_e8 br_a8" | "a8 a7"   | "a7 a8"   | "e8 c8"      || setup
+        "black king short" | "bk_e8 br_h8" | "h8 h7"   | "h7 h8"   | "e8 g8"      || setup
     }
 
-    def "cannot do white king long castle if rook moved before"() {
-        given:
-        setupPieces("wk_e1 wr_a1")
-
-        and:
-        performMove("a1 a2")
-        performMove("a2 a1")
-
-        when:
-        performMove("e1 c1")
-
-        then:
-        assertPositions("wk_e1 wr_a1")
-    }
-
-    def "cannot do black king short castle if rook moved before"() {
-        given:
-        setupPieces("bk_e8 br_h8")
-
-        and:
-        performMove("h8 h7")
-        performMove("h7 h8")
-
-        when:
-        performMove("e8 g8")
-
-        then:
-        assertPositions("bk_e8 br_h8")
-    }
-
-    def "cannot do black king long castle if rook moved before"() {
-        given:
-        setupPieces("bk_e8 br_a8")
-
-        and:
-        performMove("a8 a7")
-        performMove("a7 a8")
-
-        when:
-        performMove("e8 c8")
-
-        then:
-        assertPositions("bk_e8 br_a8")
-    }
-
-    def "can't do castling of type #type when king moved before"() {
+    def "can't do castling of type #type if king moved before"() {
         given:
         setupPieces(setup)
 
@@ -123,5 +85,27 @@ class CastlingSpec extends BaseGameSpec {
         "white king short" | "wk_e1 wr_h1" | "e1 e2"   | "e2 e1"   | "e1 g1"      || setup
         "black king long"  | "bk_e8 br_a8" | "e8 e7"   | "e7 e8"   | "e8 c8"      || setup
         "black king short" | "bk_e8 br_h8" | "e8 e7"   | "e7 e8"   | "e8 g8"      || setup
+    }
+
+    def "can do castling of type #type after undoing king move"() {
+        given:
+        setupPieces(setup)
+
+        and:
+        performMove(kingMove)
+        undoMove()
+
+        when:
+        performMove(castlingMove)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        type               | setup         | kingMove | castlingMove || positions
+        "white king long"  | "wk_e1 wr_a1" | "e1 e2"  | "e1 c1"      || "wk_c1 wr_d1"
+        "white king short" | "wk_e1 wr_h1" | "e1 e2"  | "e1 g1"      || "wk_g1 wr_f1"
+        "black king long"  | "bk_e8 br_a8" | "e8 e7"  | "e8 c8"      || "bk_c8 br_d8"
+        "black king short" | "bk_e8 br_h8" | "e8 e7"  | "e8 g8"      || "bk_g8 br_f8"
     }
 }
