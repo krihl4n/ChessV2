63cba6dd372ddc7f47877671f76fe97f96d72d7c
krihl4n <lkolasa1989@gmail.com>
2021-12-06 20:08:03 +0100

    tests for checks

--- src/test/groovy/com/krihl4n/CheckSpec.groovy
+++ src/test/groovy/com/krihl4n/CheckSpec.groovy
@@ -27,4 +27,82 @@ class CheckSpec extends BaseGameSpec {
         "wk_e4 bq_e8" | "e4 d5" || "wk_d5 bq_e8"
         "wk_e4 bq_e8" | "e4 d3" || "wk_d3 bq_e8"
     }
+
+    def "cannot move another piece leaving king checked"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e4 wq_a1 bq_e8" | "a1 a2" || startingSetup
+    }
+
+    def "cannot cause own king to be checked after other piece move"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e4 wq_e5 bq_e8" | "e5 f5" || startingSetup
+    }
+
+    def "it is safe to move other pieces until king is not attacked"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e4 wq_e5 bq_e8" | "e5 e6" || "wk_e4 wq_e6 bq_e8"
+    }
+
+    def "attack on the king can be blocked"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e4 wq_f5 bq_e8" | "f5 e5" || "wk_e4 wq_e5 bq_e8"
+    }
+
+    def "undone move can end up with king being checked"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        and:
+        undoMove()
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e4 wq_f5 bq_e8" | "f5 e5" || "wk_e4 wq_f5 bq_e8"
+    }
 }
