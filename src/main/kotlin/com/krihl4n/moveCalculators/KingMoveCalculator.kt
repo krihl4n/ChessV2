63877954461bf98c5e68901f5687ad87d016fa3b
krihl4n <lkolasa1989@gmail.com>
2021-12-07 21:45:17 +0100

    castling complex cases

--- src/main/kotlin/com/krihl4n/moveCalculators/KingMoveCalculator.kt
+++ src/main/kotlin/com/krihl4n/moveCalculators/KingMoveCalculator.kt
@@ -10,19 +10,27 @@ class KingMoveCalculator(private val positionTracker: PositionTracker, private v
     override fun calculateMoves(from: Field): Set<PossibleMove> {
         val moves = PossibleMovesCreator.create(positionTracker, from, 1, allDirectionsMoves)
         if (from == Field("e1")) {
-            if (castlingGuard.canWhiteKingLongCastle())
+            if (castlingGuard.canWhiteKingLongCastle() && positionTracker.fieldsAreEmpty("b1", "c1", "d1"))
                 moves.add(PossibleMove(Field("e1"), Field("c1")))
-            if (castlingGuard.canWhiteKingShortCastle())
+            if (castlingGuard.canWhiteKingShortCastle() && positionTracker.fieldsAreEmpty("f1", "g1"))
                 moves.add(PossibleMove(Field("e1"), Field("g1")))
         }
 
         if (from == Field("e8")) {
-            if (castlingGuard.canBlackKingLongCastle())
+            if (castlingGuard.canBlackKingLongCastle() && positionTracker.fieldsAreEmpty("b8", "c8", "d8"))
                 moves.add(PossibleMove(Field("e8"), Field("c8")))
-            if (castlingGuard.canBlackKingShortCastle())
+            if (castlingGuard.canBlackKingShortCastle() && positionTracker.fieldsAreEmpty("f8", "g8"))
                 moves.add(PossibleMove(Field("e8"), Field("g8")))
         }
 
         return moves
+    }
+
+    private fun PositionTracker.fieldsAreEmpty(vararg fields: String): Boolean {
+        fields.forEach {
+            if (!isFieldEmpty(Field(it)))
+                return false
+        }
+        return true
     }
 }
--- src/test/groovy/com/krihl4n/CastlingSpec.groovy
+++ src/test/groovy/com/krihl4n/CastlingSpec.groovy
@@ -157,6 +157,77 @@ class CastlingSpec extends BaseGameSpec {
         "black king short" | "bk_e8 br_h8" | "e8 e7"   | "e7 e8"   | "e8 e7"   | "e8 g8"      || startingSetup
     }
 
+    def "cannot castle if there are pieces between king and rook"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e1 wr_a1 wp_b1" | "e1 c1" || startingSetup
+        "wk_e1 wr_a1 wp_c1" | "e1 c1" || startingSetup
+        "wk_e1 wr_a1 wp_d1" | "e1 c1" || startingSetup
+        "wk_e1 wr_h1 wp_f1" | "e1 g1" || startingSetup
+        "wk_e1 wr_h1 wp_g1" | "e1 g1" || startingSetup
+        "bk_e8 br_a8 bp_b8" | "e8 c8" || startingSetup
+        "bk_e8 br_a8 bp_c8" | "e8 c8" || startingSetup
+        "bk_e8 br_a8 bp_d8" | "e8 c8" || startingSetup
+        "bk_e8 br_h8 bp_f8" | "e8 g8" || startingSetup
+        "bk_e8 br_h8 bp_g8" | "e8 g8" || startingSetup
+    }
+
+    def "cannot castle if king is in check"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e1 wr_a1 bq_e8" | "e1 c1" || startingSetup
+        "bk_e8 br_a8 wq_e1" | "e8 c8" || startingSetup
+    }
+
+    def "can castle if rook is under attack"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e1 wr_a1 bq_a8" | "e1 c1" || "wk_c1 wr_d1 bq_a8"
+    }
+
+    def "can castle if field adjacent to the rook is under attack"() {
+        given:
+        setupPieces(startingSetup)
+
+        when:
+        performMove(move)
+
+        then:
+        assertPositions(positions)
+
+        where:
+        startingSetup       | move    || positions
+        "wk_e1 wr_a1 bq_b8" | "e1 c1" || "wk_c1 wr_d1 bq_b8"
+        "bk_e8 br_a8 wq_b1" | "e8 c8" || "bk_c8 br_d8 wq_b1"
+    }
+
     def "cannot castle if king would be attack during move"() { // todo check if king can castle if "check"
         // todo
     }
