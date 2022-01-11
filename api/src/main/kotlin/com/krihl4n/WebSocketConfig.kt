497a133ff126d59b3f440f9c83a74bad86270638
krihl4n <lkolasa1989@gmail.com>
2022-01-11 21:34:50 +0100

    connection established/closed listener

--- api/src/main/kotlin/com/krihl4n/WebSocketConfig.kt
+++ api/src/main/kotlin/com/krihl4n/WebSocketConfig.kt
@@ -2,9 +2,13 @@ package com.krihl4n
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,5 +23,24 @@ open class WebSocketConfig: WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/game").setAllowedOriginPatterns("*")
         registry.addEndpoint("/game").setAllowedOriginPatterns("*").withSockJS()
+    }
+
+    override fun configureWebSocketTransport(registry: WebSocketTransportRegistration) {
+        registry.addDecoratorFactory { handler ->
+            object : WebSocketHandlerDecorator(handler) {
+                @Throws(Exception::class)
+                override fun afterConnectionEstablished(session: WebSocketSession) {
+                    println("connection established: $session")
+                    super.afterConnectionEstablished(session)
+                }
+
+                override fun afterConnectionClosed(session: WebSocketSession, closeStatus: CloseStatus) {
+                    println("connection closed: $session")
+                    super.afterConnectionClosed(session, closeStatus)
+                }
+            }
+        }
+
+        super.configureWebSocketTransport(registry)
     }
 }
