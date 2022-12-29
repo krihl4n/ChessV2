73d0759169064e9bca846423a36133c78ecff7ac
krihl4n <lkolasa1989@gmail.com>
2022-12-29 21:43:58 +0100

    test for game id

--- api/src/test/kotlin/ClientInteractionsSpec.kt
+++ api/src/test/kotlin/ClientInteractionsSpec.kt
@@ -15,35 +15,41 @@ class ClientInteractionsSpec : ShouldSpec({
     val eventSender = mockk<GameEventSender>(relaxed = true)
     val gameHandler = GameHandler(eventSender)
 
+    fun startGame(sessionId: String = "1111"): String{
+        return gameHandler.requestNewGame(sessionId, StartGameRequest("player1", "vs_computer", "white"))
+    }
+
     should("notify player 1 that game has started when playing vs computer") {
         val gameInfoCaptor = slot<GameInfoDto>()
         every { eventSender.gameStarted(any(), capture(gameInfoCaptor)) } returns Unit
 
-        gameHandler.requestNewGame("1234", StartGameRequest("player1", "vs_computer", "white"))
+        startGame()
 
-        verify{eventSender.gameStarted("1234", any())}
-        val capturedGameInfo = gameInfoCaptor.captured
-        assertNotEquals("1234", capturedGameInfo.gameId)
-        assertEquals("VS_COMPUTER", capturedGameInfo.mode)
-        assertEquals(PlayerDto("player1", "WHITE"), capturedGameInfo.player1)
-        assertEquals(PlayerDto("computer", "BLACK"), capturedGameInfo.player2)
+        verify { eventSender.gameStarted("1111", any()) }
+        assertNotEquals("1111", gameInfoCaptor.captured.gameId)
+        assertEquals("VS_COMPUTER", gameInfoCaptor.captured.mode)
+        assertEquals(PlayerDto("player1", "WHITE"), gameInfoCaptor.captured.player1)
+        assertEquals(PlayerDto("computer", "BLACK"), gameInfoCaptor.captured.player2)
     }
 
     should("game ids be unique") {
         val gameInfoCaptor1 = slot<GameInfoDto>()
         val gameInfoCaptor2 = slot<GameInfoDto>()
-        every { eventSender.gameStarted("1234", capture(gameInfoCaptor1)) } returns Unit
-        every { eventSender.gameStarted("1235", capture(gameInfoCaptor2)) } returns Unit
+        every { eventSender.gameStarted("1111", capture(gameInfoCaptor1)) } returns Unit
+        every { eventSender.gameStarted("2222", capture(gameInfoCaptor2)) } returns Unit
 
-        gameHandler.requestNewGame("1234", StartGameRequest("player1", "vs_computer", "white"))
-        gameHandler.requestNewGame("1235", StartGameRequest("player1", "vs_computer", "white"))
+        startGame("1111")
+        startGame("2222")
 
         assertNotEquals(gameInfoCaptor1.captured.gameId, gameInfoCaptor2.captured.gameId)
     }
 
-    should("return game id") {
-        val gameId = gameHandler.requestNewGame("1234", StartGameRequest("player1", "vs_computer", "white"))
+    should("return correct game id") {
+        val gameInfoCaptor = slot<GameInfoDto>()
+        every { eventSender.gameStarted("1111", capture(gameInfoCaptor)) } returns Unit
+        val gameId = startGame("1111")
 
         assertNotEquals("", gameId)
+        assertEquals(gameId, gameInfoCaptor.captured.gameId)
     }
 })
