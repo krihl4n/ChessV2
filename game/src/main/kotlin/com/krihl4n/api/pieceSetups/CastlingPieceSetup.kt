ace1fb35c834396b7ad35833a2cea094e1af3a74
krihl4n <krihl4n@gmail.com>
2023-02-02 19:58:41 +0100

    bug reported; castling piece setup updated

--- game/src/main/kotlin/com/krihl4n/api/pieceSetups/CastlingPieceSetup.kt
+++ game/src/main/kotlin/com/krihl4n/api/pieceSetups/CastlingPieceSetup.kt
@@ -4,6 +4,7 @@ class CastlingPieceSetup : PieceSetup {
 
     override fun get() = listOf(
         "e1 white king",
-        "h1 white rook"
+        "h1 white rook",
+        "a8 black king"
     )
 }
