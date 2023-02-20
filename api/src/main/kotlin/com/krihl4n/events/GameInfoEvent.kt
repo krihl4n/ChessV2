e82e4d5d409ab42ef5e8970d8b460528d0894231
krihl4n <krihl4n@gmail.com>
2023-02-20 19:06:51 +0100

    toString shortened on GameStartedEvent

--- api/src/main/kotlin/com/krihl4n/events/GameInfoEvent.kt
+++ api/src/main/kotlin/com/krihl4n/events/GameInfoEvent.kt
@@ -3,4 +3,9 @@ package com.krihl4n.events
 import com.krihl4n.api.dto.FieldOccupationDto
 import com.krihl4n.api.dto.PlayerDto
 
-data class GameInfoEvent(val gameId: String, val mode: String, val player: PlayerDto, val piecePositions: List<FieldOccupationDto>, val turn: String)
+data class GameInfoEvent(val gameId: String, val mode: String, val player: PlayerDto, val piecePositions: List<FieldOccupationDto>, val turn: String){
+
+    override fun toString(): String {
+        return "GameInfoEvent(gameId='$gameId', mode='$mode', player=$player, turn='$turn')"
+    }
+}
