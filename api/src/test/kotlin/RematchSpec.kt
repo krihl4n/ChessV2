11528e49fd314396bed942484f154793f0aaea64
krihl4n <krihl4n@gmail.com>
2023-05-29 19:30:03 +0200

    rematch test

--- api/src/test/kotlin/RematchSpec.kt
+++ api/src/test/kotlin/RematchSpec.kt
@@ -32,6 +32,21 @@ class RematchSpec : FunSpec({
         assertEquals(newGameId, gameInfoCaptor.captured.gameId)
     }
 
+    test("should start a new game when friend joins") {
+        val gameId = gameCommandHandler.requestNewGame(SESSION_ID_1, StartGameRequest(VS_FRIEND, null))
+        gameCommandHandler.joinGame(SESSION_ID_1, JoinGameRequest(gameId))
+        gameCommandHandler.joinGame(SESSION_ID_2, JoinGameRequest(gameId))
+        val gameInfoCaptor = gameStartedCaptor()
+
+        val newGameId = gameCommandHandler.requestRematch(SESSION_ID_1)
+        gameCommandHandler.joinGame(SESSION_ID_1, JoinGameRequest(newGameId))
+        gameCommandHandler.joinGame(SESSION_ID_2, JoinGameRequest(newGameId))
+
+        verify { msgSender.sendGameStartedMsg(SESSION_ID_1, any()) }
+        verify { msgSender.sendGameStartedMsg(SESSION_ID_2, any()) }
+        assertEquals(newGameId, gameInfoCaptor.captured.gameId)
+    }
+
     test("player should remain his id") {
         val gameInfoCaptor = gameStartedCaptor()
         val gameId = gameCommandHandler.requestNewGame(SESSION_ID_1, StartGameRequest("", null))
@@ -56,7 +71,21 @@ class RematchSpec : FunSpec({
         assertEquals(VS_COMPUTER, gameInfoCaptor.captured.mode)
     }
 
-    // should notify all players about rematch
+    test("players should change colors for rematch") {
+        val gameId = gameCommandHandler.requestNewGame(SESSION_ID_1, StartGameRequest(VS_FRIEND, null))
+        gameCommandHandler.joinGame(SESSION_ID_1, JoinGameRequest(gameId, WHITE, playerId = "p1"))
+        gameCommandHandler.joinGame(SESSION_ID_2, JoinGameRequest(gameId, playerId = "p2"))
+        val gameInfoCaptorP1 = gameStartedCaptor(SESSION_ID_1)
+        val gameInfoCaptorP2 = gameStartedCaptor(SESSION_ID_2)
+
+        val newGameId = gameCommandHandler.requestRematch(SESSION_ID_1)
+        gameCommandHandler.joinGame(SESSION_ID_1, JoinGameRequest(newGameId, playerId = "p1"))
+        gameCommandHandler.joinGame(SESSION_ID_2, JoinGameRequest(newGameId, playerId = "p2"))
+
+        assertEquals(BLACK, gameInfoCaptorP1.captured.player.color)
+        assertEquals(WHITE, gameInfoCaptorP2.captured.player.color)
+    }
+
 
 //    test("should not allow rematch if game is not finished") {
 //
