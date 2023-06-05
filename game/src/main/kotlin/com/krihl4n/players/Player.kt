34ab1c833072b9b389566eebecc8dfced67eba35
krihl4n <krihl4n@gmail.com>
2023-06-05 19:29:03 +0200

    refactor

--- game/src/main/kotlin/com/krihl4n/players/Player.kt
+++ game/src/main/kotlin/com/krihl4n/players/Player.kt
@@ -2,26 +2,4 @@ package com.krihl4n.players
 
 import com.krihl4n.model.Color
 
-class Player(val id: String, val color: Color) { // todo data class?
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Player
-
-        if (id != other.id) return false
-        if (color != other.color) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + color.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "Player(id='$id', color=$color)"
-    }
-}
+data class Player(val id: String, val color: Color)
